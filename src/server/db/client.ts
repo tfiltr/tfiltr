494449c5e735970043cad77d7dba68a1ae9946cf
Nathan Tamez <nathan@tamez.email>
2022-09-12 08:33:06 +0000

    add type gen

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,14 +1,15 @@
 // src/server/db/client.ts
 //import { PrismaClient } from "@prisma/client";
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import { Database } from "../../../supabase/types.js";
 import { env } from "../../env/server.mjs";
 
 declare global {
   // eslint-disable-next-line no-var
-  var supabase: SupabaseClient | undefined;
+  var supabase: SupabaseClient<Database> | undefined;
 }
 
-export const supabase: SupabaseClient = global.supabase ||  createClient(env.NEXT_PUBLIC_SUPABASE_URL, env.SUPABASE_SERVICE_ROLE_KEY);
+export const supabase: SupabaseClient<Database>  = global.supabase ||  createClient(env.NEXT_PUBLIC_SUPABASE_URL, env.SUPABASE_SERVICE_ROLE_KEY);
 
 if (env.NODE_ENV !== "production") {
   global.supabase = supabase;
--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
+import { Database } from '../../supabase/types.js';
 import { env } from '../env/client.mjs'
 
-export const supabase = createClient(env.NEXT_PUBLIC_SUPABASE_URL, env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+export const supabase = createClient<Database>(env.NEXT_PUBLIC_SUPABASE_URL, env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
