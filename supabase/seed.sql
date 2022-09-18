9060a6191f69d3793e71958da6a617943a910515
Nathan Tamez <nathan@tamez.email>
2022-09-18 11:04:22 +0000

    updated seed

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,5 +1,5 @@
-insert into public.example (placeId, description)
-values 
-    ('cdksjodc', 'test'),
+insert into public."Service" ("placeId", description)
+values
+ ('cdksjodc', 'test'),
     ('cscscdd','test'),
     ('csdjsjcncsn','test');
